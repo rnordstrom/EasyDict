467ec1b7b77fc17f925ea4cc1f78408158cbfaf1
rnordstrom <rikard.nordstrom94@gmail.com>
2018-08-23 11:03:34 +0200

    Project setup

---
+++ app/src/main/java/study/easydict/LookUp.java
@@ -0,0 +1,13 @@
+package study.easydict;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LookUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_look_up);
+    }
+}
